789c787af13bb258e010d5f59d08b6a7947478da
woratat <84006033+woratat@users.noreply.github.com>
2021-09-08 11:56:09 +0700

    Change create order

--- food.js
+++ food.js
@@ -2,9 +2,8 @@ var fs = require("fs");
 const { restaurants } = require("./restaurant");
 const { customers, Customer } = require("./customer");
 const { menus } = require("./menu");
+const { orders, Order } = require("./order");
 
-var orders_menu = [];
-var currentdate = new Date();
 
 getRes = () => {
   if (restaurants.length != 0) {
@@ -54,7 +53,7 @@ getOrder = (customer_id) => {
   if (reg.test(customer_id) == true) {
     let id = reg.exec(customer_id)[0];
     if (id > 0 && id <= restaurants.length) {
-      orders_menu.forEach((n) => {
+      orders.forEach((n) => {
         if (n.customer_id == customer_id) {
           data.push(n);
         }
@@ -70,31 +69,56 @@ getOrder = (customer_id) => {
 };
 
 createOrder = (json_data) => {
-  customers.forEach(x => {
-    if(x.id == json_data.customer_id){
-      json_data.order_status = "pending";
-      json_data.create_date =
-        "Date " +
-        currentdate.getDate() +
-        "-" +
-        currentdate.getMonth() +
-        "-" +
-        currentdate.getFullYear() +
-        "  Time " +
-        currentdate.getHours() +
-        ":" +
-        currentdate.getMinutes() +
-        ":" +
-        currentdate.getSeconds();
-      orders_menu.push(json_data);
-      console.log(orders_menu);
+  customers.forEach((x) => {
+    if (x.id == json_data.customer_id) {
+      orders.push(
+        new Order(json_data.customer_id, json_data.res_id, json_data.menu_id)
+      );
+      console.log(orders);
     }
-  })
+  });
 };
 
 createCustomer = (json_data) => {
-  console.log(json_data);
-  customers.push(new Customer(json_data.id, json_data.firstname, json_data.lastname, json_data.email, json_data.mobile, json_data.address));
+  let reg_name = /^[a-zA-Z ]{3,}$/;
+  let data_firstname = json_data.firstname;
+  let data_lastname = json_data.lastname;
+  if (
+    reg_name.test(data_firstname) == true &&
+    reg_name.test(data_lastname) == true
+  ) {
+    let reg_email = /^[a-zA-Z0-9.-_]+@[\w.]+\.[a-zA-Z]+$/;
+    let data_email = json_data.email;
+    if (reg_email.test(data_email) == true) {
+      let reg_mobile = /^[0][689][\d]{8}$/;
+      let data_mobile = json_data.mobile;
+      if (reg_mobile.test(data_mobile) == true) {
+        let reg_address = /[\w./() ]+/;
+        let data_address = json_data.address;
+        if (reg_address.test(data_address) == true) {
+          customers.push(
+            new Customer(
+              json_data.id,
+              json_data.firstname,
+              json_data.lastname,
+              json_data.email,
+              json_data.mobile,
+              json_data.address
+            )
+          );
+        } else {
+          throw "Error! Invalid address.";
+        }
+      } else {
+        throw "Error! Invalid mobile phone numbers.";
+      }
+    } else {
+      throw "Error! Invalid email.";
+    }
+  } else {
+    throw "Error! Invalid first name or lastname.";
+  }
+
   console.log(customers);
 };
 
